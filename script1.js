ded327c3cee7beb75855f5f3caab1b438d656061
fznyeach <ichanfauzan21@gmail.com>
2024-10-16 11:07:00 +0700

    Create script1.js

---
+++ script1.js
@@ -0,0 +1,24 @@
+function calculateBMI() {
+    const height = parseFloat(document.getElementById('height').value) / 100; // convert to meters
+    const weight = parseFloat(document.getElementById('height').value);
+
+    if (isNaN(height) || isNaN(weight) || height <= 1 || weight <= 5) {
+        document.getElementById('result').textContent = 'Tolong masukkan nilai yang valid!';
+        return;
+    }
+
+    const bmi = (weight / (height * height)).toFixed(2);
+    let category = '';
+
+    if (bmi < 18.5) {
+        category = 'Underweight';
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+        category = 'Normal weight';
+    } else if (bmi >= 25 && bmi <= 29.9) {
+        category = 'Overweight';
+    } else {
+        category = 'Obese';
+    }
+
+    document.getElementById('result').textContemt = `BMI Anda adalah ${bmi} (${category})`;
+}

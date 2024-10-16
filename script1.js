90760d45a846ee0d6b750bb3b84a024a1648786d
Lucky-Dani <lucky.saputra@student.president.ac.id>
2024-10-16 11:10:58 +0700

    Update script1.js (any mistakes when input height and weight data)

--- script1.js
+++ script1.js
@@ -2,7 +2,7 @@ function calculateBMI() {
     const height = parseFloat(document.getElementById('height').value) / 100; // convert to meters
     const weight = parseFloat(document.getElementById('height').value);
 
-    if (isNaN(height) || isNaN(weight) || height <= 1 || weight <= 5) {
+    if (isNaN(height) || isNaN(weight) || height <= 0 || weight <= 0) { //there are two errors when inputting height and weight data
         document.getElementById('result').textContent = 'Tolong masukkan nilai yang valid!';
         return;
     }
